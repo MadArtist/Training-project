7858a3135bcff7706fc5b63155c3ee0894a0f0be
MadArtist <madartist011@gmail.com>
2018-12-21 20:33:21 +0200

    html

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <link href="/css/admin.css" rel="stylesheet">
+</head>
+<body>
+
+    <section class="login">
+        <form>
+
+        </form>
+    </section>
+
+</body>
+</html>
